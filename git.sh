a597ee11e209636baa087290fd9342717f61c207
Rufas Wan <rufaswan@gmail.com>
2018-07-09 19:15:42 +0800

    BASE Update 2016-04-22

--- git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[ $# = 0 ] && exit
-git config --global "rufaswan@gmail.com"
-git remote add cmd "git@github.com:rufaswan/cmdpack.git"
-
-git add -A .
-git commit -m "$1"
-git push cmd master
